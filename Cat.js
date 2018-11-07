2ba581fdd681fc813cd0384b1d595d7b32e10d1c
Tran Van Tien <trantienhpct@gmail.com>
2018-11-07 17:28:23 +0700

    add Mouse file and update Cat file

--- Cat.js
+++ Cat.js
@@ -1,3 +1,7 @@
-let cat = function() {
-	
+let cat = function(_name, _color) {
+	this.name = _name;
+	this.color = _color;
+	this.eat: function(mouse) {
+		console.log(mouse);
+	}
 }
---
+++ Mouse.js
@@ -0,0 +1,7 @@
+let mouse = function(_name, _color) {
+	this.name = _name;
+	this.color = _color;
+	eat: function() {
+		console.log('eat chesse');
+	}
+}
