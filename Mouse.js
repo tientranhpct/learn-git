ea2e253eb520fb5e03cb7f5a82b569a7b70b4556
Tran Van Tien <trantienhpct@gmail.com>
2018-11-07 17:47:21 +0700

    add die method for mouse

--- Mouse.js
+++ Mouse.js
@@ -1,7 +1,11 @@
 let mouse = function(_name, _color) {
 	this.name = _name;
 	this.color = _color;
+	this.dead = false;
 	eat: function() {
 		console.log('eat chesse');
+	};
+	die: function() {
+		this.dead = true;
 	}
 }
