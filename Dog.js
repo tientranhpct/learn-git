f1dea32867efb27e62c6ba1683349428904fc8cd
Tran Van Tien <trantienhpct@gmail.com>
2018-11-09 08:51:23 +0700

    khoi tao 3 con cho trong file Dog.js

--- Dog.js
+++ Dog.js
@@ -5,3 +5,9 @@ let dog = function(_name, _gender) {
 		console.log('Gau Gau');
 	}
 }
+
+let pluto = new dog('Pluto', 'Male');
+let cat = new dog('Cat', 'Male');
+let pig = new dog('PPig', 'FeMale');
+
+console.log(pluto);
